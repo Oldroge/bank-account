76538f2bc26b2db36df6966b8c93cc784e04510b
oldroge <rogeriomrjr@gmail.com>
2022-08-07 14:43:38 -0300

    feat: abstraindo problemas

---
+++ ContaPoupança.java
@@ -0,0 +1,17 @@
+public class ContaPoupança {
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+}
